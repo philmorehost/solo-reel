b886edb75814cff24272123e2f5863f6199ac4c9
philmorehost <philmorehost@gmail.com>
2026-07-05 15:02:18 +0100

    Fix app icon (use logo2.png), disable resource shrinking, iOS crash safety

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
         }
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("debug")
         }
